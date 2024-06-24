5a8de1944c0ac61aac015999856d9f4a17057fb2
zjzjzjzj1874 <zjzjzjzj1874@gmail.com>
2024-06-24 11:31:37 +0800

    feat: leetcode-13:roman to int
    Signed-off-by: zjzjzjzj1874 <zjzjzjzj1874@gmail.com>

---
+++ leetcode/romanToInt_test.go
@@ -0,0 +1,25 @@
+package leetcode
+
+import "testing"
+
+func Test_romanToInt(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "romanToInt", args: args{s: "IX"}, want: 9},
+		{name: "romanToInt", args: args{s: "LVIII"}, want: 58},
+		{name: "romanToInt", args: args{s: "MCMXCIV"}, want: 1994},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := romanToInt(tt.args.s); got != tt.want {
+				t.Errorf("romanToInt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
