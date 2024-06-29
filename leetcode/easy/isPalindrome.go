7444d966af47ef6f899f1e9804af10973b639f18
zjzjzjzj1874 <zjzjzjzj1874@gmail.com>
2024-06-29 09:44:22 +0800

    feat: 234.回文链表
    Signed-off-by: zjzjzjzj1874 <zjzjzjzj1874@gmail.com>

---
+++ leetcode/easy/isPalindrome.go
@@ -0,0 +1,46 @@
+package easy
+
+// 234. 回文链表
+// 给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。如果是，返回 true ；否则，返回 false 。
+// 示例 1：
+// 输入：head = [1,2,2,1]
+// 输出：true
+// 示例 2：
+// 输入：head = [1,2]
+// 输出：false
+// 提示：
+// 链表中节点数目在范围[1, 105] 内
+// 0 <= Node.val <= 9
+// 进阶：你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+// 解题：使用数组
+func isPalindrome(head *ListNode) bool {
+	cur := head
+	n := 0
+
+	lists := make([]*ListNode, 0, 4)
+	for cur != nil {
+		lists = append(lists, cur)
+		cur = cur.Next
+		n++
+	}
+
+	for i := 0; i < n/2; i++ {
+		if lists[n-1-i].Val != head.Val {
+			return false
+		}
+		head = head.Next
+	}
+
+	return true
+}
+
+// TODO O(1)复杂度的算法：使用快慢指针，1.找到一半长度的链表；2.反转后半部分的链表；3.比较前后段部分是否相同，返回结果
---
+++ leetcode/easy/isPalindrome_test.go
@@ -0,0 +1,42 @@
+package easy
+
+import "testing"
+
+func Test_isPalindrome(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		//{name: "palindrome", args: args{head: &ListNode{
+		//	Val: 1,
+		//	Next: &ListNode{
+		//		Val: 2,
+		//		Next: &ListNode{
+		//			Val: 2,
+		//			Next: &ListNode{
+		//				Val:  1,
+		//				Next: nil,
+		//			},
+		//		},
+		//	},
+		//}}, want: true},
+		{name: "palindrome", args: args{head: &ListNode{
+			Val: 1,
+			Next: &ListNode{
+				Val:  2,
+				Next: nil,
+			},
+		}}, want: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isPalindrome(tt.args.head); got != tt.want {
+				t.Errorf("isPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
