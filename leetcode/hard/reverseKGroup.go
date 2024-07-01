2ae90ecd7e3103cfe7ebd636eca0ba29fbcc8b3e
zjzjzjzj1874 <zjzjzjzj1874@gmail.com>
2024-07-01 11:48:16 +0800

    chore: 25. K个一组翻转链表
    Signed-off-by: zjzjzjzj1874 <zjzjzjzj1874@gmail.com>

--- leetcode/hard/reverseKGroup.go
+++ leetcode/hard/reverseKGroup.go
@@ -1,6 +1,6 @@
 package hard
 
-// 25. K 个一组翻转链表 TODO 好好写写这个题，然后好好理解一下
+// 25. K 个一组翻转链表
 // 给你链表的头节点 head ，每 k 个节点一组进行翻转，请你返回修改后的链表。
 // k 是一个正整数，它的值小于或等于链表的长度。如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
 // 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
@@ -24,30 +24,27 @@ package hard
  */
 
 func reverseKGroup(head *ListNode, k int) *ListNode {
-	if k == 0 || head == nil {
+	if head == nil || k < 2 {
 		return head
 	}
-	cur := head
-	n := 0 // 求链表长度
-	for cur != nil {
-		n++
-		cur = cur.Next
+	n := 0
+	for cur := head; cur != nil; cur = cur.Next {
+		n++ // 计算链表的长度
 	}
 
-	dummy := &ListNode{Next: head} // 哨兵节点
-	var pre *ListNode              //
+	dummy := &ListNode{Next: head}
 	p0 := dummy
-	cur = p0.Next
-	for ; n >= k; n -= k { // 剩余个数大于等于K停止，每次N减少K，因为里面还有个K循环
-		for j := 0; j < k; j++ { // 每次翻转K个
+	cur := dummy.Next
+	var pre *ListNode
+
+	for ; n >= k; n -= k {
+		for i := 0; i < k; i++ { // 反转0-k，k-2k...之间的链表顺序
 			next := cur.Next
 			cur.Next = pre
 			pre = cur
-
 			cur = next
 		}
 
-		// 翻转K和K+1的节点
 		next := p0.Next
 		p0.Next.Next = cur
 		p0.Next = pre
@@ -56,3 +53,37 @@ func reverseKGroup(head *ListNode, k int) *ListNode {
 
 	return dummy.Next
 }
+
+//func reverseKGroup(head *ListNode, k int) *ListNode {
+//	if k == 0 || head == nil {
+//		return head
+//	}
+//	cur := head
+//	n := 0 // 求链表长度
+//	for cur != nil {
+//		n++
+//		cur = cur.Next
+//	}
+//
+//	dummy := &ListNode{Next: head} // 哨兵节点
+//	var pre *ListNode              //
+//	p0 := dummy
+//	cur = p0.Next
+//	for ; n >= k; n -= k { // 剩余个数大于等于K停止，每次N减少K，因为里面还有个K循环
+//		for j := 0; j < k; j++ { // 每次翻转K个
+//			next := cur.Next
+//			cur.Next = pre
+//			pre = cur
+//
+//			cur = next
+//		}
+//
+//		// 翻转K和K+1的节点
+//		next := p0.Next
+//		p0.Next.Next = cur
+//		p0.Next = pre
+//		p0 = next
+//	}
+//
+//	return dummy.Next
+//}
