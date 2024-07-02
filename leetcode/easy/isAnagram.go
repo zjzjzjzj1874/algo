1457c89f92a833a8b846760e139194a8c3706071
zjzjzjzj1874 <zjzjzjzj1874@gmail.com>
2024-07-02 09:06:42 +0800

    feat: 242.有效的字母异位词
    Signed-off-by: zjzjzjzj1874 <zjzjzjzj1874@gmail.com>

---
+++ leetcode/easy/isAnagram.go
@@ -0,0 +1,45 @@
+package easy
+
+//242. 有效的字母异位词
+//给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+//
+//注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+//
+//
+//
+//示例 1:
+//
+//输入: s = "anagram", t = "nagaram"
+//输出: true
+//示例 2:
+//
+//输入: s = "rat", t = "car"
+//输出: false
+//
+//
+//提示:
+//
+//1 <= s.length, t.length <= 5 * 104
+//s 和 t 仅包含小写字母
+//
+//
+//进阶: 如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？
+
+// 解题：仅包含小写字母，使用byte的数组来装，看最后的数量是否一致
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+
+	sArr, tArr := [26]int{}, [26]int{}
+	for i := 0; i < len(s); i++ {
+		sArr[s[i]-'a']++
+		tArr[t[i]-'a']++
+	}
+
+	if sArr == tArr {
+		return true
+	}
+
+	return false
+}
