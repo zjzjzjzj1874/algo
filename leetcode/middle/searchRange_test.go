74a9663e9f5e8ab922901a8395d4ab4e29034ca4
zjzjzjzj1874 <zjzjzjzj1874@gmail.com>
2024-07-03 11:03:26 +0800

    feat: 34. 在排序数组中查找元素的第一个和最后一个元素
    Signed-off-by: zjzjzjzj1874 <zjzjzjzj1874@gmail.com>

---
+++ leetcode/middle/searchRange_test.go
@@ -0,0 +1,27 @@
+package middle
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_searchRange(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{name: "searchRange", args: args{nums: []int{5, 7, 7, 8, 8, 10}, target: 8}, want: []int{3, 4}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := searchRange(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("searchRange() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
