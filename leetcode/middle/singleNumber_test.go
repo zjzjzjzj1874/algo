4f29eb21bdafbeb01593d2f152726ae1fd2348d8
zjzjzjzj1874 <zjzjzjzj1874@gmail.com>
2024-07-07 09:42:52 +0800

    feat: 260.只出现一次的数字III
    Signed-off-by: zjzjzjzj1874 <zjzjzjzj1874@gmail.com>

---
+++ leetcode/middle/singleNumber_test.go
@@ -0,0 +1,26 @@
+package middle
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_singleNumber(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{name: "xor", args: args{nums: []int{1, 2, 1, 3, 2, 5}}, want: []int{3, 5}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := singleNumber(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("singleNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
