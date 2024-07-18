ef27eed674aa5e48800c6cb4201bafda3a40e1ac
zjzjzjzj1874 <zjzjzjzj1874@gmail.com>
2024-07-18 14:44:48 +0800

    feat: 135.分发糖果
    Signed-off-by: zjzjzjzj1874 <zjzjzjzj1874@gmail.com>

--- leetcode/hard/firstMissingPositive_test.go
+++ leetcode/hard/firstMissingPositive_test.go
@@ -23,3 +23,28 @@ func Test_firstMissingPositive(t *testing.T) {
 		})
 	}
 }
+
+// 135. 分发糖果
+func Test_Candy(t *testing.T) {
+	type args struct {
+		ratings []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "candy", args: args{ratings: []int{1, 0, 2}}, want: 5},
+		{name: "candy", args: args{ratings: []int{1, 2, 2}}, want: 4},
+		{name: "candy", args: args{ratings: []int{1, 3, 2, 2, 1}}, want: 7},
+		{name: "candy", args: args{ratings: []int{1, 2, 87, 87, 87, 2, 1}}, want: 13},
+		{name: "candy", args: args{ratings: []int{1, 3, 4, 5, 2}}, want: 11},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := candy(tt.args.ratings); got != tt.want {
+				t.Errorf("firstMissingPositive() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
