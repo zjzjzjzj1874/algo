0ece0d5030b15439ac96ed6b9ea0e0a5e4688589
zjzjzjzj1874 <zjzjzjzj1874@gmail.com>
2024-07-24 09:48:20 +0800

    feat: lCR 081. 组合总和
    Signed-off-by: zjzjzjzj1874 <zjzjzjzj1874@gmail.com>

--- leetcode/backtrack/a_back_test.go
+++ leetcode/backtrack/a_back_test.go
@@ -5,6 +5,31 @@ import (
 	"testing"
 )
 
+// LCR 081. 组合总和
+func Test_combinationSum(t *testing.T) {
+	type args struct {
+		candidates []int
+		target     int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantAns [][]int
+	}{
+		{name: "combination", args: args{
+			candidates: []int{2, 3, 6, 7},
+			target:     7,
+		}, wantAns: [][]int{{7}, {2, 2, 3}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotAns := combinationSum(tt.args.candidates, tt.args.target); !reflect.DeepEqual(gotAns, tt.wantAns) {
+				t.Errorf("combinationSum() = %v, want %v", gotAns, tt.wantAns)
+			}
+		})
+	}
+}
+
 // LCR 083. 全排列
 func Test_permute(t *testing.T) {
 	type args struct {
