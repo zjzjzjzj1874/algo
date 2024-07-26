8fbc81b8d877e67f446b325961bfbee26fa4d68e
zjzjzjzj1874 <zjzjzjzj1874@gmail.com>
2024-07-26 18:55:28 +0800

    feat: lCR 099. 最小路径和
    Signed-off-by: zjzjzjzj1874 <zjzjzjzj1874@gmail.com>

--- leetcode/dp/a_dp_test.go
+++ leetcode/dp/a_dp_test.go
@@ -2,6 +2,27 @@ package dp
 
 import "testing"
 
+// LCR 099. 最小路径和
+func Test_minPathSum(t *testing.T) {
+	type args struct {
+		grid [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "minPathSum", args: args{grid: [][]int{{1, 3, 1}, {1, 5, 1}, {4, 2, 1}}}, want: 7},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minPathSum(tt.args.grid); got != tt.want {
+				t.Errorf("minPathSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 // LCR 088. 使用最小花费爬楼梯
 func Test_minCostClimbingStairs(t *testing.T) {
 	type args struct {
