a126b832c08b53e01320c4a2db1f801d5a4ee378
zjzjzjzj1874 <zjzjzjzj1874@gmail.com>
2024-07-26 19:07:12 +0800

    feat: lCR 098.不同路径
    Signed-off-by: zjzjzjzj1874 <zjzjzjzj1874@gmail.com>

--- leetcode/dp/a_dp_test.go
+++ leetcode/dp/a_dp_test.go
@@ -2,6 +2,29 @@ package dp
 
 import "testing"
 
+// LCR 098. 不同路径
+func Test_uniquePaths(t *testing.T) {
+	type args struct {
+		m int
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "uniquePath", args: args{m: 3, n: 7}, want: 28},
+		{name: "uniquePath", args: args{m: 7, n: 3}, want: 28},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := uniquePaths(tt.args.m, tt.args.n); got != tt.want {
+				t.Errorf("uniquePaths() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 // LCR 099. 最小路径和
 func Test_minPathSum(t *testing.T) {
 	type args struct {
