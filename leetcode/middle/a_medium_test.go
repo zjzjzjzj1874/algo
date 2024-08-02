d065e295180db4d053dfdedb4a5ac408f6c927b7
zjzjzjzj1874 <zjzjzjzj1874@gmail.com>
2024-08-02 17:43:25 +0800

    feat: 3128. 直角三角形
    Signed-off-by: zjzjzjzj1874 <zjzjzjzj1874@gmail.com>

--- leetcode/middle/a_medium_test.go
+++ leetcode/middle/a_medium_test.go
@@ -6,6 +6,27 @@ import (
 	"testing"
 )
 
+// 3128. 直角三角形
+func Test_numberOfRightTriangles(t *testing.T) {
+	type args struct {
+		grid [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		{name: "numberOfRightTriangles", args: args{grid: [][]int{{0, 1, 0}, {0, 1, 1}, {0, 1, 0}}}, want: 2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numberOfRightTriangles(tt.args.grid); got != tt.want {
+				t.Errorf("numberOfRightTriangles() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 // 2125. 银行中的激光束数量
 func Test_numberOfBeams(t *testing.T) {
 	type args struct {
