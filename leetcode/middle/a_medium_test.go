34f3b91ec2a95d10d2d1d3b58e6d9a232d37908f
zjzjzjzj1874 <zjzjzjzj1874@gmail.com>
2024-08-05 15:30:14 +0800

    feat: 50. pow求幂
    Signed-off-by: zjzjzjzj1874 <zjzjzjzj1874@gmail.com>

--- leetcode/middle/a_medium_test.go
+++ leetcode/middle/a_medium_test.go
@@ -6,6 +6,30 @@ import (
 	"testing"
 )
 
+// 50. Pow(x, n)
+func Test_myPow(t *testing.T) {
+	type args struct {
+		x float64
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{name: "myPow", args: args{x: 2, n: 10}, want: 1024},
+		{name: "myPow", args: args{x: 2, n: 9}, want: 512},
+		{name: "myPow", args: args{x: 2, n: 19}, want: 524288},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := myPow(tt.args.x, tt.args.n); got != tt.want {
+				t.Errorf("myPow() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 // 3143. 正方形中的最多点数
 func Test_maxPointsInsideSquare(t *testing.T) {
 	type args struct {
