2c4fef765daaaba4b8b7fd41deda0f2f42f60340
zjzjzjzj1874 <zjzjzjzj1874@gmail.com>
2024-08-12 09:12:42 +0800

    feat: 1768. 交替合并字符串
    Signed-off-by: zjzjzjzj1874 <zjzjzjzj1874@gmail.com>

---
+++ leetcode/doublePtr/README.md
@@ -0,0 +1,4 @@
+
+## 双指针题型
+
+* [1768. 交替合并字符串](https://leetcode.cn/problems/merge-strings-alternately/description/?envType=study-plan-v2&envId=leetcode-75)
---
+++ leetcode/doublePtr/a_double_ptr_test.go
@@ -0,0 +1,25 @@
+package doublePtr
+
+import "testing"
+
+// 1768. 交替合并字符串
+func Test_mergeAlternately(t *testing.T) {
+	type args struct {
+		word1 string
+		word2 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{name: "merge", args: args{word1: "abc", word2: "pqr"}, want: "apbqcr"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := mergeAlternately(tt.args.word1, tt.args.word2); got != tt.want {
+				t.Errorf("mergeAlternately() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
