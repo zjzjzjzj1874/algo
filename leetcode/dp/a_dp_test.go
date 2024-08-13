20b72fe2c750ad8462a3b0610f83bf98a8371b79
zjzjzjzj1874 <zjzjzjzj1874@gmail.com>
2024-08-13 15:47:38 +0800

    feat: 650.两个键的键盘
    Signed-off-by: zjzjzjzj1874 <zjzjzjzj1874@gmail.com>

--- leetcode/dp/a_dp_test.go
+++ leetcode/dp/a_dp_test.go
@@ -2,6 +2,31 @@ package dp
 
 import "testing"
 
+// 650. 两个键的键盘
+func Test_minSteps(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "min steps", args: args{n: 5}, want: 5},
+		{name: "min steps", args: args{n: 6}, want: 5},
+		{name: "min steps", args: args{n: 7}, want: 7},
+		{name: "min steps", args: args{n: 8}, want: 6},
+		{name: "min steps", args: args{n: 9}, want: 6},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minSteps(tt.args.n); got != tt.want {
+				t.Errorf("minSteps() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 // 2684. 矩阵中移动的最大次数
 func Test_maxMoves(t *testing.T) {
 	type args struct {
