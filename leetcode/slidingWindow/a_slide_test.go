3e439dd51b78751ff3d5ef312898f8b5efab9108
zjzjzjzj1874 <zjzjzjzj1874@gmail.com>
2024-08-15 11:14:31 +0800

    feat: 1456.定长子串中元音的最大数目
    Signed-off-by: zjzjzjzj1874 <zjzjzjzj1874@gmail.com>

--- leetcode/slidingWindow/a_slide_test.go
+++ leetcode/slidingWindow/a_slide_test.go
@@ -1,2 +1,26 @@
 package slidingWindow
 
+import "testing"
+
+// 1456. 定长子串中元音的最大数目
+func Test_maxVowels(t *testing.T) {
+	type args struct {
+		s string
+		k int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "maxVowels", args: args{s: "abciiidef", k: 3}, want: 3},
+		{name: "maxVowels", args: args{s: "novowels", k: 1}, want: 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxVowels(tt.args.s, tt.args.k); got != tt.want {
+				t.Errorf("maxVowels() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
