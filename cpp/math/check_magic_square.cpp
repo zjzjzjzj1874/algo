e742c2c8e2747e73ed0b8ccb2c049e021e7f918d
zjzjzjzj1874 <zjzjzjzj1874@gmail.com>
2024-11-06 14:44:53 +0800

    perf: optimize range code
    Signed-off-by: zjzjzjzj1874 <zjzjzjzj1874@gmail.com>

--- cpp/math/check_magic_square.cpp
+++ cpp/math/check_magic_square.cpp
@@ -15,40 +15,62 @@ bool check_magic_square(const std::vector<std::vector<int>>& s){
     }
     // 计算目标值，每行每列相加之和=>等差数列求和(1+n*n)*(n*n)/2/n,化简如下。
     int target = (1+n*n)*n/2;
+//    // 校验每行
+//    for (int i = 0; i < n; i++) {
+//        int sum = 0;
+//        for (int j = 0; j < n; j++) {
+//            sum += s[i][j];
+//        }
+//        if (sum != target) {
+//            return false;
+//        }
+//    }
+//    // 校验每列
+//    for (int i = 0; i < n; i++) {
+//        int sum = 0;
+//        for (int j = 0; j < n; j++) {
+//            sum += s[j][i];
+//        }
+//        if (sum != target) {
+//            return false;
+//        }
+//    }
+//    // 校验对角线1
+//    int s1 = 0;
+//    for (int i = 0; i < n; i++) {
+//        s1 += s[i][i];
+//    }
+//    if (s1 != target) {
+//        return false;
+//    }
+//    // 校验对角线2
+//    int s2 = 0;
+//    for (int i = 0; i < n; i++) {
+//        s2 += s[i][n-i-1];
+//    }
+//    if (s2 != target) {
+//        return false;
+//    }
+
+    // 优化
+
+    // 校验两条对角线
+    int sumDiag1 = 0, sumDiag2 = 0;
     // 校验每行
     for (int i = 0; i < n; i++) {
-        int sum = 0;
+        int sumCol = 0, sumRow = 0;
         for (int j = 0; j < n; j++) {
-            sum += s[i][j];
+            sumRow += s[i][j];
+            sumCol += s[j][i];
         }
-        if (sum != target) {
+        if (sumCol != target || sumRow != target) {
             return false;
         }
+
+        sumDiag1 += s[i][i];
+        sumDiag2 += s[i][n-i-1];
     }
-    // 校验每列
-    for (int i = 0; i < n; i++) {
-        int sum = 0;
-        for (int j = 0; j < n; j++) {
-            sum += s[j][i];
-        }
-        if (sum != target) {
-            return false;
-        }
-    }
-    // 校验对角线1
-    int s1 = 0;
-    for (int i = 0; i < n; i++) {
-        s1 += s[i][i];
-    }
-    if (s1 != target) {
-        return false;
-    }
-    // 校验对角线2
-    int s2 = 0;
-    for (int i = 0; i < n; i++) {
-        s2 += s[i][n-i-1];
-    }
-    if (s2 != target) {
+    if (sumDiag1 != target || sumDiag2 != target ) {
         return false;
     }
 
