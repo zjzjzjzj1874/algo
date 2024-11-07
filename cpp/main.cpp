42101f1439d66eb6436cc70b0d453ac3511d1393
zjzjzjzj1874 <zjzjzjzj1874@gmail.com>
2024-11-07 10:45:56 +0800

    feat: rotate image in cpp
    Signed-off-by: zjzjzjzj1874 <zjzjzjzj1874@gmail.com>

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -5,17 +5,58 @@
 #include <iostream>
 #include <vector>
 #include "math/prime.h"
+#include "math/rotate_image.h"
 #include "math/check_magic_square.h"
 #include "math/add_string.h"
 #include "sort/sort.h"
 #include "reverse.h"
 
 using namespace std;
+//
+// // 函数声明
+// void rotate_image(int** arr, int n);
 
 /*
  * cpp算法的入口
  */
 int main() {
+    int n = 3; // 数组的大小
+
+    // 动态分配二维数组
+    int** arr = new int*[n];
+    for (int i = 0; i < n; ++i) {
+        arr[i] = new int[n];
+    }
+
+    // 填充数组
+    int value = 1;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            arr[i][j] = value++;
+        }
+    }
+
+    std::cout << "原始数组:" << std::endl;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            std::cout << arr[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    // 调用旋转函数
+    rotate_image(arr, n);
+
+    std::cout << "旋转后的数组:" << std::endl;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            std::cout << arr[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+
+
     string num1 = "123", num2 = "11";
     // string num1 = "56", num2 = "77";
     string sum = add_string_without_vector(num1, num2);
@@ -66,3 +107,25 @@ int main() {
     }
     cout << endl;
 }
+
+// 旋转图像的函数定义
+// void rotate_image(int** arr, int n) {
+//     // 进行 90 度顺时针旋转
+//     for (int layer = 0; layer < n / 2; ++layer) {
+//         int first = layer;
+//         int last = n - layer - 1;
+//         for (int i = first; i < last; ++i) {
+//             int offset = i - first;
+//             // 保存顶部
+//             int top = arr[first][i];
+//             // 左到顶
+//             arr[first][i] = arr[last - offset][first];
+//             // 底到左
+//             arr[last - offset][first] = arr[last][last - offset];
+//             // 右到底
+//             arr[last][last - offset] = arr[i][last];
+//             // 顶到右
+//             arr[i][last] = top;
+//         }
+//     }
+// }
---
+++ cpp/math/rotate_image.h
@@ -0,0 +1,13 @@
+//
+// Created by zjzjzjzj1874 on 2024/11/7.
+//
+
+#ifndef ROTATE_IMAGE_H
+#define ROTATE_IMAGE_H
+
+
+// 将图像顺时针旋转90度；转置矩阵
+void rotate_image(int** arr, int n);
+
+
+#endif //ROTATE_IMAGE_H
